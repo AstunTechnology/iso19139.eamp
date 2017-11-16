186e157774f324bf6863d7c5e9d9e7e14e11843c
archaeogeek <jo.k.cook@gmail.com>
2017-11-16 10:34:06 +0000

    Initial changes to start eamp- folder structure, pom, readme

--- src/main/plugin/iso19139.gemini22/formatter/full_view/view.groovy
+++
@@ -1,7 +0,0 @@
-import iso19139.SummaryFactory
-
-def isoHandlers = new iso19139.Handlers(handlers, f, env)
-
-SummaryFactory.summaryHandler({it.parent() is it.parent()}, isoHandlers)
-
-isoHandlers.addDefaultHandlers()
